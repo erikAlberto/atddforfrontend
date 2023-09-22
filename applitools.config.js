abf23edec0de24784d380474a34865a64bc7a39c
Alberto Vargas <erikalberto8492@gmail.com>
2023-09-22 11:00:03 -0400

    fixed github actions file

---
+++ applitools.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testConcurrency: 1,
+    apiKey: '',
+    batchName: 'cypress-POC',
+    browser: [
+        {width: 800, height: 600, name: 'chrome'},
+        {deviceName: 'Pixel 2', screenOrientation: 'portrait'}
+    ]
+}
